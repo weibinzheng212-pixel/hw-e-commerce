9a015a2466e3d265d306ddbf9486de9a123a0770
weibinzheng212-pixel <weibinzheng212@gmail.com>
2026-05-26 11:49:22 -0400

    rename

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import "@mantine/core/styles.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import AuthProvider from "./features/auth/pages/AuthContext";
+import AuthProvider from "./features/auth/pages/Authcontext";
 
 function App() {
   // weibin
