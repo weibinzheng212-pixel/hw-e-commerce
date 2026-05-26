246b068147c224a7ff206b423f03bc5446d266d9
weibinzheng212-pixel <weibinzheng212@gmail.com>
2026-05-26 12:12:59 -0400

    settings page updated

--- client/src/features/settings/pages/Settings.tsx
+++ client/src/features/settings/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from "@mantine/core";
+import { Button, Container, Flex, Space, TextInput, Title } from "@mantine/core";
 
 const Settings = () => {
   return (
@@ -6,6 +6,49 @@ const Settings = () => {
       <Title order={1} mb="xl">
         Settings
       </Title>
+
+      <form>
+        <div style={{ display: "flex" }}>
+          <TextInput
+            label="First name"
+            placeholder="first name "
+            required
+            mt="md"
+            autoComplete="nope"
+          />
+          <Space w="md" />
+          <TextInput
+            label="last name "
+            placeholder="last name"
+            required
+            mt="md"
+            autoComplete="nope"
+          />
+        </div>
+
+        <div style={{ display: "flex" }}>
+          <TextInput
+            label="Username"
+            placeholder="jane@example.com"
+            required
+            mt="md"
+            autoComplete="nope"
+          />
+          <Space w="md" />
+
+          <TextInput
+            label="Email"
+            placeholder="email"
+            required
+            mt="md"
+            autoComplete="nope"
+          />
+        </div>
+          <TextInput label="phone" placeholder="phone number" required mt="md" autoComplete="nope" />
+          <br />
+
+          <Button>Save changes</Button>
+      </form>
     </Container>
   );
 };
